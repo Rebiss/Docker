67cb1c1788b2086ee71d47ac0d95cf371b72d55f
Andranik Barseghyan <barsegyanat.93@gmail.com>
2020-04-12 23:42:27 +0400

    add: .env.example, rout

---
+++ Heroku/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app     = express();
+
+const PORT = process.env.PORT || 8080;
+const HOST = process.env.HOST || 'localhost';
+
+app.get('/', (req,res) => res.end('<p> <h1> Root </h1> </p>'));
+app.get('/urs', (req,res) => res.end('<p> User </p>'));
+
+app.listen(PORT, HOST, () => {
+    console.log(` Server run ${HOST}:${PORT} `)
+})
